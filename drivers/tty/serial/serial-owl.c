d5da4d2c40f7d177ecda8e983b40d91c4b3a407a
huangjunmin <huangjunmin@actions-semi.com>
2018-03-10 15:21:27 +0800

    serial-owl: suppress message
    suppress message "cannot get the RX(TX) DMA channel!"
    
    Change-Id: I6cc1ac59f5e85f46ec9a7f2b22a7d56c1c6d1c2a

--- drivers/tty/serial/serial-owl.c
+++ drivers/tty/serial/serial-owl.c
@@ -756,7 +756,7 @@ static int owl_serial_dma_init_rx(struct owl_uart_port *aport)
 	/* init for RX */
 	aport->rx_dma_chan = dma_request_slave_channel(dev, "rx");
 	if (!aport->rx_dma_chan) {
-		dev_info(dev, "cannot get the RX DMA channel!\n");
+		dev_dbg(dev, "cannot get the RX DMA channel!\n");
 		return -ENODEV;
 	}
 
@@ -797,7 +797,7 @@ static int owl_serial_dma_init_tx(struct owl_uart_port *aport)
 	/* init for TX */
 	aport->tx_dma_chan = dma_request_slave_channel(dev, "tx");
 	if (!aport->tx_dma_chan) {
-		dev_info(dev, "cannot get the TX DMA channel!\n");
+		dev_dbg(dev, "cannot get the TX DMA channel!\n");
 		return -ENODEV;
 	}
 
